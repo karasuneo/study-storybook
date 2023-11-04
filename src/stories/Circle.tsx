52e3cf97b9a578bc8f39da2f47f7c46b27a80bab
Yusei Mizutani <yusei4520030401@gmail.com>
2023-11-04 16:40:35 +0900

    storybook

---
+++ src/stories/Circle.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+type Props = {
+  variant: "green" | "yellow" | "orange";
+};
+
+
+/**
+ * This is a story for the Circle component.
+ */
+const Circle = ({ variant }: Props) => {
+  let bgColor;
+
+  switch (variant) {
+    case "orange":
+      bgColor = "bg-orange-500";
+      break;
+    case "yellow":
+      bgColor = "bg-yellow-500";
+      break;
+    case "green":
+      bgColor = "bg-green-500";
+      break;
+    default:
+      bgColor = "bg-orange-500";
+  }
+
+  return <div className={`${bgColor} w-14 h-14 p-2 rounded-full`}></div>;
+};
+
+export default Circle;
